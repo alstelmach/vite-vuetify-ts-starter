73f08c59f5b9b2d75d0159a5d81913df1690c424
alstelmach <astelmach@astelmach.dev>
2023-04-16 17:28:02 +0200

    Added exemplary unit test and GitHub Actions workflow to run tests

---
+++ src/components/AppBarMenuComponent/__tests__/AppBarMenuComponent.spec.ts
@@ -0,0 +1,50 @@
+﻿import { describe, it, expect, vi } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+import { createTestingPinia } from '@pinia/testing';
+import AppBarMenuComponent from '../AppBarMenuComponent.vue';
+import { getUserConfigStore } from '@/store';
+
+describe('AppBarMenuComponent', () => {
+  mockWindowMatchMedia();
+
+  const testingPinia = createTestingPinia({
+    createSpy: vi.fn(),
+  });
+
+  const userConfigStore = getUserConfigStore(testingPinia);
+  userConfigStore._darkThemeModeEnabled = true;
+  userConfigStore._currentLocale = 'en';
+
+  const wrappedComponent = shallowMount(AppBarMenuComponent, {
+    global: {
+      plugins: [testingPinia],
+    },
+  });
+
+  it('renders theme toggle button', () => {
+    const themeToggleButton = wrappedComponent.find('.v-btn');
+    expect(themeToggleButton).toBeTruthy();
+  });
+
+  it('updates document title on mounted hook', () => {
+    expect(document.title).toBe('Robercik');
+  });
+
+  wrappedComponent.unmount();
+});
+
+function mockWindowMatchMedia(): void {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: vi.fn().mockImplementation(query => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    })),
+  });
+}
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,40 +1,45 @@
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+
 import { fileURLToPath, URL } from 'node:url';
 
-import { configDefaults, defineConfig } from 'vitest/config';
-import { mergeConfig } from 'vite';
-import viteConfig from './vite.config';
-/**
- * Vitest Configure
- *
- * @see {@link https://vitest.dev/config/}
- */
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    // plugins
-    plugins: [
-      {
-        name: 'vitest-plugin-beforeall',
-        config: () => ({
-          test: {
-            setupFiles: [
-              fileURLToPath(new URL('./vitest/beforeAll.ts', import.meta.url)),
-            ],
-          },
-        }),
-      },
-    ],
-    test: {
-      // https://vitest.dev/guide/#configuring-vitest
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      globalSetup: [
-        fileURLToPath(new URL('./vitest/setup.ts', import.meta.url)),
-      ],
-      deps: {
-        inline: ['vuetify'],
-      },
+export default defineConfig({
+  // Resolver
+  resolve: {
+    // https://vitejs.dev/config/#resolve-alias
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  })
-);
+  },
+  // plugins
+  plugins: [
+    {
+      name: 'vitest-plugin-beforeall',
+      config: () => ({
+        test: {
+          setupFiles: [
+            fileURLToPath(new URL('./vitest/beforeAll.ts', import.meta.url)),
+          ],
+        },
+      }),
+    } as any,
+    // Vue3
+    vue(),
+    // Vuetify Loader
+    // https://github.com/vuetifyjs/vuetify-loader
+    vuetify({
+      autoImport: true,
+      styles: { configFile: './src/styles/variables.scss' },
+    }),
+  ],
+  test: {
+    // https://vitest.dev/guide/#configuring-vitest
+    globals: true,
+    globalSetup: [fileURLToPath(new URL('./vitest/setup.ts', import.meta.url))],
+    environment: 'jsdom',
+    deps: {
+      inline: ['vuetify'],
+    },
+  },
+});
